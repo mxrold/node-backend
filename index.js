1cbf860532408f9820cb90093543b96aec8f82c1
Martin <martinroldan889@gmail.com>
2021-09-16 12:48:47 -0300

    Platzivideo CRUD

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const { config } = require('./movies-api/config/')
+const { moviesApi } = require('./movies-api/routes/movies')
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
-
-app.get('/json', function(req, res) {
-  res.json({ hello: 'World!' })
-})
+moviesApi(app)
 
 app.listen(config.port, function() {
   console.log(`Listening: http://localhost:${config.port}`)
